41b9a39a7953c23cc2c0438deae93881ec527271
Brenno C. Lins <dev@bcl-st.com.br>
2021-12-28 07:56:50 -0300

    Desafio 01 - Conceitos do React [OK]

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,37 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle == "") {
+      return
+    }
+
+    const taskId = Math.floor(Math.random() * 1000 + 1);
+    
+    const newTask = {
+      id: taskId,
+      title: newTaskTitle,
+      isComplete: false
+    }
+    
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updatedTask = tasks.map( task => task.id == id 
+      ? { ...task, isComplete: !task.isComplete } //pego a task e autero apenas o campo isComplete
+      : task
+    )
+
+    setTasks(updatedTask)
+    
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const deletedTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(deletedTasks);
   }
 
   return (
